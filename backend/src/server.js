9727468365bdad36723632ab13fe98d196656f2a
YosephAwoke <yosephawoke8@gmail.com>
2025-05-22 12:55:16 +0300

    Ready for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+
 import notesRoutes from "./routes/notesRoutes.js";
 // import bodyParser from "body-parser";
 // import { connect } from "mongoose";
@@ -11,21 +13,31 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware
-app.use(cors({
-  origin: "http://localhost:5173", // Allow requests from this origin
-  
-}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // Allow requests from this origin
+    })
+  );
+}
+
 app.use(express.json()); // Parse JSON bodies
 app.use(rateLimiter);
 
-
 app.use("/api/notes", notesRoutes);
 
-app.get("/api/notes", (req, res) => {
-  res.send("Hello from the serverr");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+// Connect to MongoDB
 
 connectDB().then(() => {
   app.listen(PORT, () => {
